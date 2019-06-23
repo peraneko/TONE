0eb6f4866e168bc54045bdbeeb233caca551ac67
ぺらねこ <duelist.info@gmail.com>
2019-06-23 21:09:41 +0900

    Update keymap.c

--- tone/keymaps/test/keymap.c
+++ tone/keymaps/test/keymap.c
@@ -18,7 +18,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT( 
         KC_A,       KC_B,      KC_C,     KC_D,\
-        KC_E,       KC_F,      KC_G,     KC_H,\
+        KC_E,       KC_F,      KC_G,     KC_H
     )
 };
 
