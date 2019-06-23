91dee19630a4fd61984bc0d9999f522c495ca2c2
ぺらねこ <duelist.info@gmail.com>
2019-06-23 21:07:17 +0900

    Update keymap.c

--- tone/keymaps/test/keymap.c
+++ tone/keymaps/test/keymap.c
@@ -19,7 +19,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT( 
         KC_A,       KC_B,      KC_C,     KC_D,\
         KC_E,       KC_F,      KC_G,     KC_H,\
-        _______,    _______ // Set with encoder_update_user () 
     )
 };
 
