572fc8c4a98bc1805ac7274a8669aa86ac2e0c76
ぺらねこ <duelist.info@gmail.com>
2019-08-05 13:37:03 +0900

    Update keymap.c

--- tone/keymaps/test/keymap.c
+++ tone/keymaps/test/keymap.c
@@ -25,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Rotary encoder settings */
 void encoder_update_user(uint16_t index, bool clockwise) {
    if (clockwise) {
-        tap_code(KC_0);    //Rotary encoder clockcwise
+        tap_code(KC_0);    //Rotary encoder clockwise
     } else {
-        tap_code(KC_1);  //Rotary encoder Reverse clockcwise
+        tap_code(KC_1);  //Rotary encoder Reverse clockwise
     }
 }
